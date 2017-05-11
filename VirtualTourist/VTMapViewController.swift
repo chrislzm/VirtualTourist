1bff956868e4aa7f81f913d80fdd82e858eb5ea0
chrislzm <me@chrisleung.com>
2017-05-11 16:32:15 -0700

    feat: Added long press gesture recognizer and action to add annotation

--- VirtualTourist/VTMapViewController.swift
+++ VirtualTourist/VTMapViewController.swift
@@ -11,6 +11,8 @@ import MapKit
 
 class VTMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +23,15 @@ class VTMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func longPressOnMap(_ gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+        }
+    }
 
 }
 
