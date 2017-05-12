7f656585db6220ea25a15c879ed66c9cd5af9463
chrislzm <me@chrisleung.com>
2017-05-11 17:38:45 -0700

    feat: Added main model interface and first method that I need to implement

---
+++ VirtualTourist/VTModel.swift
@@ -0,0 +1,15 @@
+//
+//  VTModel.swift
+//  VirtualTourist
+//
+//  Created by Chris Leung on 5/11/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import Foundation
+
+class VTModel {
+    func createNewPin(lat:Double, long:Double) {
+        
+    }
+}
