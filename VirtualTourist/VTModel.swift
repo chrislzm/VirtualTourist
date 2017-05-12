208930b56166d0c0a5ca41ff2a4e7eb3c121d53a
chrislzm <me@chrisleung.com>
2017-05-12 02:40:22 -0700

    refactor: modularized methods into multiple smaller ones

--- VirtualTourist/VTModel.swift
+++ VirtualTourist/VTModel.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class VTModel {
     
-    func createNewPin(lat:Double, long:Double, completionHandler: @escaping (_ error: String?, _ pin: Pin?) -> Void) {
+    func createNewPin(lat:Double, long:Double) -> Pin  {
         // Get the stack
         let delegate = UIApplication.shared.delegate as! AppDelegate
         let stack = delegate.stack
@@ -22,30 +22,49 @@ class VTModel {
         // Save the pin
         stack.save()
         
-        // Send the pin to the completion handler
-        completionHandler(nil,newPin)
+        return newPin
+    }
+    
+    func loadNewPhotoURLsFor(_ pin: Pin, completionHandler: @escaping (_ error: String?) -> Void) {
         
+        // Get the stack
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let stack = delegate.stack
+        
+        // If we have more pages we can get, set to the next page
+        if (pin.photosPageNum < pin.photosTotalPages) {
+            pin.photosPageNum += 1
+        } else {
+            // Otherwise set back to getting photos on page 1
+            pin.photosPageNum = 1
+        }
+
         // Get photo URLs from the network client class
-        VTNetClient.sharedInstance().getPhotoURLs(lat: lat, long: long, pageNumber: 1) { (error,photoURLs) in
+        VTNetClient.sharedInstance().getPhotoURLs(lat: pin.latitude, long: pin.longitude, pageNumber: Int(pin.photosPageNum)) { (error,photoURLs,totalPages) in
             
             // If there was an error, pass the message to the controller
             guard error == nil else {
-                completionHandler(error,nil)
+                completionHandler(error)
                 return
             }
-
-            // If there was no error message, we can safely unwrap
+            
+            // If there was no error message, we can safely unwrap the URL
             let photoURLs = photoURLs!
+            
+            // Update the total number of pages
+            pin.photosTotalPages = Int16(totalPages!)
             
             if photoURLs.count > 0 {
                 // Create model objects for each photo, and attach it to the pin
                 for photoURL in photoURLs {
                     let newPhoto = Photo(url: photoURL, context: stack.context)
-                    newPhoto.pin = newPin
+                    newPhoto.pin = pin
                     print("Added photoURL into Pin! \(photoURL)")
                 }
                 // Save the URLs
                 stack.save()
+                
+                completionHandler(nil)
             } else {
                 // TODO: Remove debug statement, do we need to do something if there are no photos?
                 print("No photos in this area")
@@ -77,7 +96,7 @@ class VTModel {
         }
     }
     
-    func getFRCAndLoadImagesFor(_ pin: Pin) -> NSFetchedResultsController<NSFetchRequestResult> {
+    func getFrcFor(_ pin: Pin) -> NSFetchedResultsController<NSFetchRequestResult> {
         
         // Get the stack
         let delegate = UIApplication.shared.delegate as! AppDelegate
@@ -100,8 +119,18 @@ class VTModel {
             print("Error while trying to perform a search: \n\(e)\n\(frc)")
         }
 
+        // Return the FetchedResultsController
+        return frc
+    }
+    
+    func loadImagesFor(_ frc:NSFetchedResultsController<NSFetchRequestResult>) {
+        
+        // Get the stack
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let stack = delegate.stack
+        
         let photos = frc.fetchedObjects as! [Photo]
-
+        
         stack.performBackgroundBatchOperation { (context) in
             for photo in photos {
                 if photo.imageData == nil {
@@ -117,9 +146,6 @@ class VTModel {
                 }
             }
         }
-
-        // Return the FetchedResultsController
-        return frc
     }
     
     // MARK: Shared Instance
