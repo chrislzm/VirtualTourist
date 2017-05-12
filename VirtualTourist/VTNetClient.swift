c373040312ec05f2669a8bc8ea53a2e610d66914
chrislzm <me@chrisleung.com>
2017-05-11 19:40:10 -0700

    feat: Added network client to handle HTTP method requests

---
+++ VirtualTourist/VTNetClient.swift
@@ -0,0 +1,112 @@
+//
+//  VTNetClient.swift
+//  VirtualTourist
+//
+//  Core client methods for VT (Virtual Tourist)
+//
+//  Created by Chris Leung on 5/11/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import Foundation
+
+class VTNetClient {
+    
+    // MARK: Methods
+    
+    // Shared method for all HTTP requests
+    
+    func taskForHTTPMethod(apiParameters: [String:Any], completionHandler: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
+ 
+        /* 1. Create Session and Request */
+
+        let session = URLSession.shared
+        let request = URLRequest(url: createURLFromParameters(apiParameters))
+        
+        /* 2. Make the request */
+
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandler(nil, NSError(domain: "taskForHTTPMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(String(describing: error))")
+                return
+            }
+            
+            /* GUARD: Did we get a status code from the response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                sendError("Your request did not return a valid response (no status code).")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard statusCode >= 200 && statusCode <= 299  else {
+                sendError("Your request returned a status code other than 2xx. Status code returned: \(statusCode)")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            /* 4/5. Parse the data and use the data (happens in completion handler) */
+            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandler)
+        }
+        
+        /* 6. Start the request */
+        task.resume()
+        
+        return task
+    }
+    
+    // MARK: Private helper methods
+    
+    // MARK: Helper for Creating a URL from Parameters
+    
+    private func createURLFromParameters(_ parameters: [String:Any]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.ApiScheme
+        components.host = Constants.ApiHost
+        components.path = Constants.ApiPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+    
+    // Converts raw JSON into a usable Foundation object and sends it to a completion handler
+    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(parsedResult, nil)
+    }
+    
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> VTNetClient {
+        struct Singleton {
+            static var sharedInstance = VTNetClient()
+        }
+        return Singleton.sharedInstance
+    }
+}
