56f7fa0136c94109b2a0ba7efe138107a5f543cf
chrislzm <me@chrisleung.com>
2017-05-11 18:00:45 -0700

    feat: Created initializer for Pin

--- VirtualTourist/Pin+CoreDataClass.swift
+++ VirtualTourist/Pin+CoreDataClass.swift
@@ -12,4 +12,15 @@ import CoreData
 @objc(Pin)
 public class Pin: NSManagedObject {
 
+    // MARK: Initializer
+    
+    convenience init(lat: Double, long: Double, context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.latitude = lat
+            self.longitude = long
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 }
