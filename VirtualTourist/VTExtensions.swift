14aae413b9851b22e079945e6b452930de430270
chrislzm <me@chrisleung.com>
2017-05-12 00:25:43 -0700

    feat: Subclassed MKPointAnnotation so that we can store the Pin object inside the annotation itself

---
+++ VirtualTourist/VTExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  VTExtensions.swift
+//  VirtualTourist
+//
+//  Created by Chris Leung on 5/11/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import MapKit
+
+class VTMKPointAnnotation : MKPointAnnotation {
+    var pin:Pin?
+}
