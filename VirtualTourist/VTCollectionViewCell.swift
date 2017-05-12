94800f4b7922d189da49841fcdd1b81fb33bcd25
chrislzm <me@chrisleung.com>
2017-05-12 00:25:03 -0700

    feat: Added collection view cell with image

---
+++ VirtualTourist/VTCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  VTCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Chris Leung on 5/11/17.
+//  Copyright © 2017 Chris Leung. All rights reserved.
+//
+
+import UIKit
+
+class VTCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+}
