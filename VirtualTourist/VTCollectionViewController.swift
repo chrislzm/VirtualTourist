7236943c76c92f9423e84242f554bdaf8f8aeb54
chrislzm <me@chrisleung.com>
2017-05-11 21:07:57 -0700

    feat+refactor: display the coordinate in the map view on load, now storing coordinate instead of individual lat/long properties

--- VirtualTourist/VTCollectionViewController.swift
+++ VirtualTourist/VTCollectionViewController.swift
@@ -7,8 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class VTCollectionViewController : UIViewController {
-    var latitude:Double?
-    var longitude:Double?
+    
+    // MARK: Properties
+    var coordinate:CLLocationCoordinate2D?
+    
+    // MARK: Outlets
+    @IBOutlet weak var mapView: MKMapView!
+
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Add the coordinate as an annotation to the map
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate!
+        mapView.addAnnotation(annotation)
+        
+        // Set the MapView to a 1km * 1km box around the geocoded location
+        let viewRegion = MKCoordinateRegionMakeWithDistance(coordinate!, 1000, 1000);
+        mapView.setRegion(viewRegion, animated: true)
+    }
 }
