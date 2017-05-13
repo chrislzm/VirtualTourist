05dd5d3b318e8a8f40395b9fbc07d4094a61c1e5
chrislzm <me@chrisleung.com>
2017-05-12 23:46:55 -0700

    doc: comments

--- VirtualTourist/VTControllerExtensions.swift
+++ VirtualTourist/VTControllerExtensions.swift
@@ -1,6 +1,8 @@
 //
-//  VTExtensions.swift
+//  VTControllerExtensions.swift
 //  VirtualTourist
+//
+//  Extensions related to the Virtual Tourist Controller classes
 //
 //  Created by Chris Leung on 5/11/17.
 //  Copyright © 2017 Chris Leung. All rights reserved.
@@ -8,6 +10,7 @@
 
 import MapKit
 
+// Subclass the MapKit Point Annotation so that it can store the Pin object itself
 class VTMKPointAnnotation : MKPointAnnotation {
     var pin:Pin?
 }
