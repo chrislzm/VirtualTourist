3b0014b84d00fd3f78f990ab849013a953d42ce3
chrislzm <me@chrisleung.com>
2017-05-13 17:52:42 -0700

    refactor: -separated create new photo into its own method that saves every time a photo is created (better, to prevent inconsistencies when modifying context and photo has been created but not saved) -return new photo objects to the completion handler, saving the controller the trouble of fetching the new photo objects itself -shuffled around methods for clarity doc: added comments

--- VirtualTourist/VTModel.swift
+++ VirtualTourist/VTModel.swift
@@ -15,30 +15,58 @@ class VTModel {
 
 
     // Creates new Pin object with given latitude and longitude, saves it to the model and returns it
-
+    
     func createNewPin(lat:Double, long:Double) -> Pin  {
-
         let coreDataStack = getCoreDataStack()
-        
         let newPin = Pin(lat: lat,long: long,context: coreDataStack.context)
-        
         coreDataStack.save()
-        
         return newPin
     }
 
-    // Loads new Photo objects for a given Pin. Note that this loads photo URLs from Flickr, creates and saves Photo objects, but does not load their image data. That is done by calling the loadImagesFor(::) method.
-    // Calls completionHandler when finished. error will be nil if there was no error, otherwise it will contain the error message.
+    // Creates new Photo object for a given pin and URL and saves it to the model and returns it
     
-    func loadNewPhotosFor(_ pin: Pin, completionHandler: @escaping (_ error: String?) -> Void) {
-        
+    func createNewPhoto(_ photoPin:Pin, _ photoURL:String) -> Photo {
+        let coreDataStack = getCoreDataStack()
+        let newPhoto = Photo(pin:photoPin, url: photoURL, context: coreDataStack.context)
+        coreDataStack.save()
+        return newPhoto
+    }
+    
+    // Creates a Fetch Results Controller for a given pin and returns it. (The FRC will fetch the pin's Photo objects.)
+    
+    func createFrcFor(_ pin: Pin) -> NSFetchedResultsController<NSFetchRequestResult> {
         let coreDataStack = getCoreDataStack()
         
+        // Create Fetch Request
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Photo")
+        fr.sortDescriptors = [NSSortDescriptor(key: "url", ascending: false)]
+        let pred = NSPredicate(format: "pin = %@", argumentArray: [pin])
+        fr.predicate = pred
+        
+        // Create FetchedResultsController
+        let frc = NSFetchedResultsController(fetchRequest: fr, managedObjectContext:coreDataStack.context, sectionNameKeyPath: nil, cacheName: nil)
+        do {
+            try frc.performFetch()
+        } catch let e as NSError {
+            fatalError("Unable to access data. Error returned: \(e)")
+        }
+        
+        // Return the FetchedResultsController
+        return frc
+    }
+
+    // Loads new Photo objects for a given Pin. Note that this loads photo URLs from Flickr, creates and saves Photo objects, but does not load their image data. That is done by calling the loadImagesFor(::) method.
+    
+    // Calls completionHandler when finished. error parameter will be nil if there was no error, otherwise it will contain the error message. New photo objects will be contained in the newPhotos parameter.
+    
+    func loadNewPhotosFor(_ pin: Pin, completionHandler: @escaping (_ newPhotos:[Photo]?, _ error: String?) -> Void) {
+
         // If we have more pages of photos we can get from Flickr, increment to the next page
         if (pin.photosPageNum < pin.photosTotalPages) {
             pin.photosPageNum += 1
         } else {
-            // Otherwise set back to page 1
+            
+            // Otherwise set page to 1
             pin.photosPageNum = 1
         }
 
@@ -47,118 +75,37 @@ class VTModel {
             
             // If there was an error, pass the message to the controller
             guard error == nil else {
-                completionHandler(error)
+                completionHandler(nil,error)
                 return
             }
             
             // If there was no error message, we can safely unwrap the URL
             let photoURLs = photoURLs!
             
-            // Update the total number of pages
+            // Update the total number of pages available in the Pin
             pin.photosTotalPages = Int16(totalPages!)
             
             // If there are photos in this area
             if photoURLs.count > 0 {
-                // Create model objects for each photo, and attach it to the pin
+                var newPhotos = [Photo]()
                 for photoURL in photoURLs {
-                    let newPhoto = Photo(url: photoURL, context: coreDataStack.context)
-                    newPhoto.pin = pin
+                    newPhotos.append(self.createNewPhoto(pin, photoURL))
                 }
-                // Save the URLs
-                coreDataStack.save()
                 
-                // Return with no error
-                completionHandler(nil)
+                // Return photos
+                completionHandler(newPhotos,nil)
             }
         }
     }
     
-    func getSavedPins() -> [Pin]? {
-        
-        let coreDataStack = getCoreDataStack()
-        
-        // Create a fetchrequest
-        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
-        
-        // Execute the request
-        var results:[Any]?
-        do {
-            results = try coreDataStack.context.fetch(fr)
-        } catch {
-            fatalError("Error retrieving saved pins")
-        }
-        
-        if let savedPins = results as? [Pin] {
-            return savedPins
-        } else {
-            return nil
-        }
-    }
+    // Downloads image data from Flickr for a given set of Photo objects. Runs in the background. If the Photo object already has image data, it does not download it again.
     
-    func createFrcFor(_ pin: Pin) -> NSFetchedResultsController<NSFetchRequestResult> {
-
-        let coreDataStack = getCoreDataStack()
-        
-        // Create Fetch Request
-        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Photo")
-        
-        fr.sortDescriptors = [NSSortDescriptor(key: "url", ascending: false)]
-        
-        let pred = NSPredicate(format: "pin = %@", argumentArray: [pin])
-        fr.predicate = pred
-        
-        // Create FetchedResultsController
-        let frc = NSFetchedResultsController(fetchRequest: fr, managedObjectContext:coreDataStack.context, sectionNameKeyPath: nil, cacheName: nil)
-        
-        do {
-            try frc.performFetch()
-        } catch let e as NSError {
-            print("Error while trying to perform a search: \n\(e)\n\(frc)")
-        }
-
-        // Return reference to the FetchedResultsController
-        return frc
-    }
-
-    // Returns the Core Data Stack
-    func getCoreDataStack() -> CoreDataStack {
-        let delegate = UIApplication.shared.delegate as! AppDelegate
-        return delegate.stack
-    }
-    
-    // Delete all photos in given array of photos from Core Data Stack
-    func deleteAll(_ photos:[Photo]) {
-        let coreDataStack = getCoreDataStack()
-        let context = coreDataStack.context
-        for photo in photos {
-            context.delete(photo)
-        }
-        coreDataStack.save()
-    }
-
-    func deletePhoto(_ photo:Photo)
-    {
-        let coreDataStack = getCoreDataStack()
-        let context = coreDataStack.context
-        context.delete(photo)
-        coreDataStack.save()
-    }
-    
-    func deletePin(_ pin:Pin) {
-        let coreDataStack = getCoreDataStack()
-        let context = coreDataStack.context    
-        context.delete(pin)
-        coreDataStack.save()
-    }
+    // Calls completionHandler when finished. error parameter will be nil if there was no error, otherwise it will contain the error message.
     
     func loadImagesFor(_ photos:[Photo], completionHandler: @escaping (_ error: String?) -> Void) {
-
         let coreDataStack = getCoreDataStack()
-        
         coreDataStack.performBackgroundBatchOperation { (context) in
-            
             var error:String? = nil
-            
             for photo in photos {
                 if photo.imageData == nil {
                     let imageURL = URL(string: photo.url!)
@@ -173,6 +120,68 @@ class VTModel {
         }
     }
     
+    // Returns all pins stored in persistent data
+    
+    func getSavedPins() -> [Pin]? {
+        let coreDataStack = getCoreDataStack()
+        
+        // Create a fetchrequest
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
+        
+        // Execute the request
+        var results:[Any]?
+        do {
+            results = try coreDataStack.context.fetch(fr)
+        } catch {
+            fatalError("Error retrieving saved pins")
+        }
+        
+        // If we have results and they are of type Pin, return them
+        if let savedPins = results as? [Pin] {
+            return savedPins
+        } else {
+            return nil
+        }
+    }
+
+    // Delete all photos in given array of photos from Core Data Stack
+    
+    func deleteAll(_ photos:[Photo]) {
+        let coreDataStack = getCoreDataStack()
+        let context = coreDataStack.context
+        for photo in photos {
+            context.delete(photo)
+        }
+        coreDataStack.save()
+    }
+
+    // Delete single photo from the Core Data Stack
+    
+    func deletePhoto(_ photo:Photo)
+    {
+        let coreDataStack = getCoreDataStack()
+        let context = coreDataStack.context
+        context.delete(photo)
+        coreDataStack.save()
+    }
+    
+    // Delete a Pin from the Core Data Stack
+    
+    func deletePin(_ pin:Pin) {
+        let coreDataStack = getCoreDataStack()
+        let context = coreDataStack.context    
+        context.delete(pin)
+        coreDataStack.save()
+    }
+
+    // MARK: Helper Functions
+    
+    func getCoreDataStack() -> CoreDataStack {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.stack
+    }
+    
+
     // MARK: Shared Instance
     
     class func sharedInstance() -> VTModel {
